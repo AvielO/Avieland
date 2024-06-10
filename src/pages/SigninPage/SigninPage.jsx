229257b4a54262515737fcd10202a6368b9de9b2
Aviel <aviell1100611@gmail.com>
2024-06-11 01:31:36 +0300

    Final design of signin screen - for NOW

--- src/pages/SigninPage/SigninPage.jsx
+++ src/pages/SigninPage/SigninPage.jsx
@@ -1,18 +1,28 @@
+import { useNavigate } from "react-router-dom";
+import styles from "./SigninPage.module.css";
+
 const SigninPage = () => {
+  const navigate = useNavigate();
+
+  const handleSignupClick = (e) => {
+    e.preventDefault();
+    navigate("/signup");
+  };
+
   return (
     //justify-center - Vertically - אנכי
     //items-center - Horizontally - אופקי
 
     <div className="grid h-screen grid-cols-2">
-      <div className="bg-sky-200 flex flex-col items-center justify-center gap-6">
-        <p className="text-sky-600 mb-4 text-7xl font-semibold underline md:text-8xl lg:mb-10 lg:text-9xl">
+      <div className="bg-sky-200 flex flex-col items-center justify-center gap-6 bg-cover bg-center">
+        <p className="text-sky-600 mb-4 text-7xl font-semibold underline md:text-8xl lg:mb-10 lg:text-[168px]">
           כניסה
         </p>
 
         <img
           src="/user-icon.png"
           alt="User Image"
-          className="h-20 w-20 md:h-24 md:w-24 lg:h-48 lg:w-48"
+          className="h-20 w-20 md:h-24 md:w-24 lg:h-64 lg:w-64"
         />
 
         <form className="flex flex-col items-center gap-4">
@@ -21,7 +31,7 @@ const SigninPage = () => {
             <input
               type="text"
               name="name"
-              className="rounded border p-2 lg:h-11 lg:w-96"
+              className="rounded border p-2 lg:h-12 lg:w-[42rem]"
               required
             />
           </div>
@@ -31,37 +41,40 @@ const SigninPage = () => {
             <input
               type="password"
               name="password"
-              className="rounded border p-2 lg:h-11 lg:w-96"
+              className="rounded border p-2 lg:h-12 lg:w-[42rem]"
               required
             />
           </div>
 
           <div>
-            <button className="bg-sky-600 text-sky-50 mt-8 rounded px-4 py-2">
+            <button className="bg-sky-600 text-sky-50 mt-8 rounded px-6 py-4 font-semibold">
               התחברות
             </button>
           </div>
         </form>
       </div>
 
-      <div className="bg-sky-50 flex flex-col justify-center items-center">
-        <h1 className="text-sky-600 mx-4 mt-12 text-5xl lg:mx-8 lg:text-7xl">
+      <div className="bg-sky-50 flex flex-col items-center justify-center">
+        <h1 className="text-sky-600 mx-4 mt-12 text-5xl lg:mx-8 lg:text-8xl">
           ברוכים הבאים לאביאלנד
         </h1>
-        <p className="text-md mx-4 mt-8 lg:mx-8 lg:text-2xl">
+        <p className="text-md mx-4 mt-8 lg:mx-8 lg:text-3xl">
           משחק דפדפן אינטרנט בו תוכלו ליצור את הצבא שלכם.
           <br />
           משחק שבו תוכלו להתחרות בחברים בטקטיקת המשחק היחודית שלכם.
           <br />
           הירשמו עכשיו ונסו להיות האדם בעל הצבא החזק במשחק!
         </p>
-        <button className="bg-sky-600 text-sky-50 mx-4 mt-6 h-10 w-32 rounded lg:mx-8 lg:h-12 lg:w-40">
+        <button
+          onClick={(e) => handleSignupClick(e)}
+          className="bg-sky-600 text-sky-50 mx-4 mt-6 h-10 w-32 rounded font-semibold lg:mx-8 lg:h-12 lg:w-40"
+        >
           הרשמה
         </button>
         <img
           src="/swords.png"
           alt="User Image"
-          className="h-96 w-96 md:h-96 md:w-96 lg:h-[30rem] lg:w-[30rem]"
+          className="h-96 w-96 md:h-96 md:w-96 lg:h-[32rem] lg:w-[32rem]"
         />
       </div>
     </div>
