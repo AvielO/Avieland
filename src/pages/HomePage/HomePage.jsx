19d6e7cd4703d663df28d216f59f2a3c1dd2f482
Aviel <aviell1100611@gmail.com>
2024-06-19 19:40:02 +0300

    Data that need to be in all pages moved to AppLayout instead of homePage

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,19 +1,5 @@
-import Sidebar from "../../components/Sidebar/Sidebar";
-import Header from "../../components/Header/Header";
-import UserResources from "../../components/UserResources/UserResources";
-
 const HomePage = () => {
-  return (
-    <div className="flex h-full flex-row bg-sky-200">
-      <div className="w-[40dvh] bg-sky-50">
-        <Sidebar />
-      </div>
-      <div className="w-full m-10">
-        <Header />
-        <UserResources />
-      </div>
-    </div>
-  );
+  return <div>HomePage</div>;
 };
 
 export default HomePage;
