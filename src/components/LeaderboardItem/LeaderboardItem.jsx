5c420d4980058336133c3345a034ee0fb3f7dd0b
Aviel <aviell1100611@gmail.com>
2024-06-21 19:25:54 +0300

    Leaderboard is work and designed with fake data

---
+++ src/components/LeaderboardItem/LeaderboardItem.jsx
@@ -0,0 +1,31 @@
+const translations = {
+  attacker: "/player-type-icons/attacker-icon.png",
+  defender: "/player-type-icons/defender-icon.png",
+  attdefer: "/player-type-icons/attdefer-icon.png",
+};
+
+const LeaderboardItem = ({
+  index,
+  username,
+  type,
+  gold,
+  soliders,
+  workers,
+  group,
+}) => {
+  return (
+    <tr className="h-9 text-center">
+      <td>{index}</td>
+      <td>{username}</td>
+      <td className="flex items-center justify-center">
+        <img className="h-16 w-16" src={translations[type]} alt="player-type" />
+      </td>
+      <td>{gold}</td>
+      <td>{soliders}</td>
+      <td>{workers}</td>
+      <td>{group || "אין"}</td>
+    </tr>
+  );
+};
+
+export default LeaderboardItem;
--- src/pages/Leaderboard/Leaderboard.jsx
+++ src/pages/Leaderboard/Leaderboard.jsx
@@ -1,41 +1,89 @@
+import LeaderboardItem from "../../components/LeaderboardItem/LeaderboardItem";
+
 const Leaderboard = () => {
   //UseEffect for fetch data of all players
   //Need to think which information I want to dispaly
   //Design each column, Blue, White, Blue - Like that.
   //Optional - sort of the list.DF
   return (
-    <div>
-      <table>
-        <thead>
-          <tr>
+    <>
+      <table className="w-full">
+        <thead className="h-10 bg-sky-300 text-xl">
+          <tr className="text-sky-900">
             <th>#</th>
             <th>שם משתמש</th>
+            <th>סוג</th>
             <th>זהב</th>
             <th>לוחמים</th>
+            <th>עובדים</th>
+            <th>אירגון</th>
           </tr>
         </thead>
-        <tbody>
-          <tr>
-            <th>1</th>
-            <th>אביאל</th>
-            <th>10005</th>
-            <th>50</th>
+        <tbody className="bg-sky-100 text-2xl text-sky-700">
+          
+          <tr className="h-9 text-center">
+            <td>1</td>
+            <td>אביאל</td>
+            <td className="flex items-center justify-center">
+              <img
+                className="h-16 w-16"
+                src="/player-type-icons/attacker-icon.png"
+                alt="player-type"
+              />
+            </td>
+            <td>10005</td>
+            <td>50</td>
+            <td>50</td>
+            <td>אין</td>
           </tr>
-          <tr>
-            <th>2</th>
-            <th>עילית</th>
-            <th>8346</th>
-            <th>40</th>
+          <tr className="h-9 bg-sky-50 text-center">
+            <td>2</td>
+            <td>נועה</td>
+            <td className="flex items-center justify-center">
+              <img
+                className="h-16 w-16"
+                src="/player-type-icons/attdefer-icon.png"
+                alt="player-type"
+              />
+            </td>
+            <td>9783</td>
+            <td>43</td>
+            <td>43</td>
+            <td>אין</td>
           </tr>
-          <tr>
-            <th>3</th>
-            <th>שלומקה</th>
-            <th>3526</th>
-            <th>23</th>
+          <tr className="h-9 text-center">
+            <td>3</td>
+            <td>עילית</td>
+            <td className="flex items-center justify-center">
+              <img
+                className="h-16 w-16"
+                src="/player-type-icons/defender-icon.png"
+                alt="player-type"
+              />
+            </td>
+            <td>8346</td>
+            <td>40</td>
+            <td>40</td>
+            <td>אין</td>
+          </tr>
+          <tr className="h-9 bg-sky-50 text-center">
+            <td>4</td>
+            <td>שלומקה</td>
+            <td className="flex items-center justify-center">
+              <img
+                className="h-16 w-16"
+                src="/player-type-icons/attdefer-icon.png"
+                alt="player-type"
+              />
+            </td>
+            <td>3526</td>
+            <td>23</td>
+            <td>23</td>
+            <td>אין</td>
           </tr>
         </tbody>
       </table>
-    </div>
+    </>
   );
 };
 
