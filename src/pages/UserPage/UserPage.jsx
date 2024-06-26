a20959e9a2c7571d60d6da299249a2fe9224840b
Aviel <aviell1100611@gmail.com>
2024-06-26 23:07:20 +0300

    first design of right side of player inspect

--- src/pages/UserPage/UserPage.jsx
+++ src/pages/UserPage/UserPage.jsx
@@ -1,6 +1,10 @@
 import { useLoaderData } from "react-router-dom";
 import { getUserInformation } from "../../services/backendAPI.js";
 
+import { MdOutlineDriveFileRenameOutline } from "react-icons/md";
+import { MdOutlineGroups } from "react-icons/md";
+import { VscTypeHierarchy } from "react-icons/vsc";
+
 const UserPage = () => {
   const userDetails = useLoaderData();
   //Maybe add created day account
@@ -10,9 +14,40 @@ const UserPage = () => {
   */
 
   return (
-    <>
-      <span>UserPage: {userDetails.id}</span>
-    </>
+    <div className="mx-6 grid h-fit grid-cols-2 gap-10">
+      <div className="flex flex-col justify-center gap-4 rounded-xl bg-sky-300 py-2">
+        <h1 className="text-center text-5xl">מידע על המשתמש X</h1>
+        <hr />
+        <div className="flex flex-col">
+          <div className="flex items-center justify-center gap-6">
+            <img
+              className="h-64 w-64"
+              src="/user-icon.png"
+              alt="user-picture"
+            />
+            <div className="flex flex-col gap-8 text-4xl">
+              <div className="flex items-center gap-4">
+                <MdOutlineDriveFileRenameOutline />
+                <span className="">אביאלו</span>
+              </div>
+
+              <div className="flex items-center gap-4">
+                <MdOutlineGroups />
+                <span className="">המננצחים</span>
+              </div>
+
+              <div className="flex items-center gap-4">
+                <VscTypeHierarchy />
+                <span className="">סוג גברי</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="flex justify-center rounded-xl bg-sky-100 py-2">
+        <h1 className="text-6xl">סטטיסטיקות</h1>
+      </div>
+    </div>
   );
 };
 
