4c7f985d500f863891bbb93b153b262714f2cb9e
Aviel <aviell1100611@gmail.com>
2024-07-04 23:15:37 +0300

    first design with fake data of hire works

--- src/pages/HirePage/HirePage.jsx
+++ src/pages/HirePage/HirePage.jsx
@@ -1,9 +1,9 @@
 const HirePage = () => {
   return (
-    <div className="">
+    <div className="flex flex-col gap-4">
       <section className="flex flex-col items-center gap-6">
         <h1 className="text-5xl font-semibold underline">העסק חיילים</h1>
-        <div className="flex w-fit flex-col items-center gap-2 rounded-3xl bg-sky-100 p-4">
+        <div className="flex w-fit flex-col items-center gap-2 rounded-3xl bg-sky-100 p-4 drop-shadow-md">
           <img
             className="h-64 w-56 rounded-3xl"
             src="background-login.png"
@@ -29,35 +29,101 @@ const HirePage = () => {
             <input className="h-6 w-12 rounded-full text-center" />
           </div>
         </div>
+        <div>
+          <button className="rounded-2xl bg-sky-300 px-4 py-2 text-2xl">
+            העסק חיילים
+          </button>
+        </div>
       </section>
 
-      <section className="flex flex-col gap-8">
-        <h1 className="text-5xl font-semibold">העסק חיילים</h1>
-        <div className="flex w-fit flex-col items-center gap-2 rounded-3xl bg-sky-100 p-4">
-          <img
-            className="h-64 w-56 rounded-3xl"
-            src="background-login.png"
-            alt="solider-background"
-          />
+      <section className="flex flex-col items-center gap-6">
+        <h1 className="text-5xl font-semibold underline">העסק עובדים</h1>
+        <div className="flex w-full justify-around">
+          <div className="flex w-fit flex-col items-center gap-2 rounded-3xl bg-sky-100 p-4 drop-shadow-md">
+            <img
+              className="h-64 w-56 rounded-3xl"
+              src="background-login.png"
+              alt="solider-background"
+            />
 
-          <span className="text-2xl font-semibold">חייל התקפי</span>
+            <span className="text-2xl font-semibold">עובד זהב</span>
 
-          <div className="flex flex-col items-center">
-            <span className="text-2xl font-semibold">עלות</span>
-            <div className="flex items-center">
-              <img
-                className="h-8 w-8"
-                src="/resources-icons/gold-icon.png"
-                alt="gold-resource-icon"
-              />
-              <span className="text-2xl">100</span>
+            <div className="flex flex-col items-center">
+              <span className="text-2xl font-semibold">עלות</span>
+              <div className="flex items-center">
+                <img
+                  className="h-8 w-8"
+                  src="/resources-icons/gold-icon.png"
+                  alt="gold-resource-icon"
+                />
+                <span className="text-2xl">100</span>
+              </div>
+            </div>
+
+            <div className="flex items-center gap-1 text-xl">
+              <label>כמות: </label>
+              <input className="h-6 w-12 rounded-full text-center" />
             </div>
           </div>
 
-          <div className="flex items-center gap-1 text-xl">
-            <label>כמות: </label>
-            <input className="h-6 w-12 rounded-full text-center" />
+          <div className="flex w-fit flex-col items-center gap-2 rounded-3xl bg-sky-100 p-4 drop-shadow-md">
+            <img
+              className="h-64 w-56 rounded-3xl"
+              src="background-login.png"
+              alt="solider-background"
+            />
+
+            <span className="text-2xl font-semibold">עובד כסף</span>
+
+            <div className="flex flex-col items-center">
+              <span className="text-2xl font-semibold">עלות</span>
+              <div className="flex items-center">
+                <img
+                  className="h-8 w-8"
+                  src="/resources-icons/silver-icon.png"
+                  alt="silver-resource-icon"
+                />
+                <span className="text-2xl">100</span>
+              </div>
+            </div>
+
+            <div className="flex items-center gap-1 text-xl">
+              <label>כמות: </label>
+              <input className="h-6 w-12 rounded-full text-center" />
+            </div>
           </div>
+
+          <div className="flex w-fit flex-col items-center gap-2 rounded-3xl bg-sky-100 p-4 drop-shadow-md">
+            <img
+              className="h-64 w-56 rounded-3xl"
+              src="background-login.png"
+              alt="solider-background"
+            />
+
+            <span className="text-2xl font-semibold">עובד נחושת</span>
+
+            <div className="flex flex-col items-center">
+              <span className="text-2xl font-semibold">עלות</span>
+              <div className="flex items-center">
+                <img
+                  className="h-8 w-8"
+                  src="/resources-icons/copper-icon.png"
+                  alt="copper-resource-icon"
+                />
+                <span className="text-2xl">100</span>
+              </div>
+            </div>
+
+            <div className="flex items-center gap-1 text-xl">
+              <label>כמות: </label>
+              <input className="h-6 w-12 rounded-full text-center" />
+            </div>
+          </div>
+        </div>
+        <div>
+          <button className="rounded-2xl bg-sky-300 px-4 py-2 text-2xl">
+            העסק עובדים
+          </button>
         </div>
       </section>
     </div>
