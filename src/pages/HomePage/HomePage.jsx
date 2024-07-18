a7702f9d8d14ae6a49f2a36d700728238159f1a4
Aviel <aviell1100611@gmail.com>
2024-07-19 02:00:50 +0300

    designfix - Bars was on the same line on small devices

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,6 +1,18 @@
 import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
-import { PieChart, Pie, Tooltip, BarChart, Bar, XAxis, Legend } from "recharts";
+import {
+  PieChart,
+  Pie,
+  Tooltip,
+  BarChart,
+  Bar,
+  XAxis,
+  Legend,
+  Cell,
+} from "recharts";
+
+const ATTACKE_DEFENDER_COLORS = ["#FF4500", "#32CD32"];
+const RESOURCES_COLORS = ["#B87333", "#C0C0C0", "#FFD700"];
 
 const HomePage = () => {
   const username = useSelector((state) => state.user.username);
@@ -24,85 +36,99 @@ const HomePage = () => {
   }, []);
 
   return (
-    <div>
-      <div className="flex justify-center">
-        <div className="flex flex-col items-center">
-          <span>כמות הפועלים</span>
-          <PieChart width={500} height={400}>
-            <Pie
-              dataKey="value"
-              data={distributions.workersDistribution}
-              // outerRadius={80}
-              fill="#8884d8"
-            />
-            <Tooltip />
-          </PieChart>
-        </div>
-        <div className="flex flex-col items-center">
-          <span>כוח עצמי</span>
-          <PieChart width={500} height={400}>
-            <Pie
-              dataKey="value"
-              data={distributions.playerPowerDistribution}
-              // outerRadius={80}
-              fill="#8884d8"
-            />
-            <Tooltip />
-          </PieChart>
-        </div>
-        <div className="flex flex-col items-center">
-          <span>כמות דוחות</span>
-          <BarChart
-            width={500}
-            height={400}
-            data={distributions.reportsTypeDistribution}
+    <div className="flex flex-wrap justify-center p-4">
+      <div className="m-2 flex h-fit flex-col items-center rounded-3xl bg-sky-50 p-4">
+        <span className="text-4xl font-semibold">כמות הפועלים</span>
+        <PieChart width={400} height={370}>
+          <Pie
+            dataKey="value"
+            data={distributions.workersDistribution}
+            outerRadius={160}
+            fill="#8884d2"
           >
-            <XAxis dataKey="name" />
-            <Tooltip />
-            <Bar yAxisId="attacker-defender" dataKey="attacker" fill="#8884d8" />
-            <Bar yAxisId="attacker-defender" dataKey="defender" fill="#82ca9d" />
-          </BarChart>
-        </div>
+            {distributions.workersDistribution.map((_, index) => (
+              <Cell
+                key={`cell-${index}`}
+                fill={RESOURCES_COLORS[index % RESOURCES_COLORS.length]}
+              />
+            ))}
+          </Pie>
+          <Tooltip />
+        </PieChart>
       </div>
-      <div className="flex justify-center">
-        <div className="flex flex-col items-center">
-          <span>כמות נצחונות והפסדים</span>
-          <BarChart
-            width={500}
-            height={400}
-            data={distributions.reportsWinLoseDistribution}
+      <div className="m-2 flex h-fit flex-col items-center rounded-3xl bg-sky-50 p-4">
+        <span className="text-4xl font-semibold">מדד כוח</span>
+        <PieChart width={400} height={370}>
+          <Pie
+            dataKey="value"
+            data={distributions.playerPowerDistribution}
+            outerRadius={160}
+            fill="#8884d8"
           >
-            <XAxis dataKey="name" />
-            <Tooltip />
-            <Legend />
-            <Bar dataKey="wins" stackId="win-lose" fill="#8884d8" />
-            <Bar dataKey="loses" stackId="win-lose" fill="#82ca9d" />
-          </BarChart>
-        </div>
-        <div className="flex flex-col items-center">
-          <span>כמות נשקים</span>
-          <PieChart width={500} height={400}>
-            <Pie
-              dataKey="value"
-              data={distributions.weaponsDistribution}
-              // outerRadius={80}
-              fill="#8884d8"
-            />
-            <Tooltip />
-          </PieChart>
-        </div>
-        <div className="flex flex-col items-center">
-          <span>כמות משאבים בבנק</span>
-          <PieChart width={500} height={400}>
-            <Pie
-              dataKey="value"
-              data={distributions.workersDistribution}
-              // outerRadius={80}
-              fill="#8884d8"
-            />
-            <Tooltip />
-          </PieChart>
-        </div>
+            {distributions.playerPowerDistribution.map((_, index) => (
+              <Cell
+                key={`cell-${index}`}
+                fill={
+                  ATTACKE_DEFENDER_COLORS[
+                    index % ATTACKE_DEFENDER_COLORS.length
+                  ]
+                }
+              />
+            ))}
+          </Pie>
+          <Tooltip />
+        </PieChart>
+      </div>
+      <div className="m-2 flex h-fit flex-col items-center rounded-3xl bg-sky-50 p-4">
+        <span className="text-4xl font-semibold">כמות דוחות</span>
+        <BarChart
+          width={400}
+          height={370}
+          data={distributions.reportsTypeDistribution}
+        >
+          <XAxis dataKey="name" />
+          <Tooltip />
+          <Bar yAxisId="attacker-defender" dataKey="attacker" fill="#8884d8" />
+          <Bar yAxisId="attacker-defender" dataKey="defender" fill="#82ca9d" />
+        </BarChart>
+      </div>
+
+      <div className="m-2 flex h-fit flex-col items-center rounded-3xl bg-sky-50 p-4">
+        <span className="text-4xl font-semibold">כמות נצחונות והפסדים</span>
+        <BarChart
+          width={400}
+          height={370}
+          data={distributions.reportsWinLoseDistribution}
+        >
+          <XAxis dataKey="name" />
+          <Tooltip />
+          <Bar dataKey="wins" stackId="win-lose" fill="#8884d8" />
+          <Bar dataKey="loses" stackId="win-lose" fill="#82ca9d" />
+        </BarChart>
+      </div>
+      <div className="m-2 flex h-fit flex-col items-center rounded-3xl bg-sky-50 p-4">
+        <span className="text-4xl font-semibold">כמות נשקים</span>
+        <PieChart width={400} height={370}>
+          <Pie
+            dataKey="value"
+            data={distributions.weaponsDistribution}
+            outerRadius={160}
+            fill="#8884d8"
+          />
+          <Tooltip />
+        </PieChart>
+      </div>
+      <div className="m-2 flex h-fit flex-col items-center rounded-3xl bg-sky-50 p-4">
+        <span className="text-4xl font-semibold">כמות משאבים בבנק</span>
+        <PieChart width={400} height={370}>
+          <Pie
+            dataKey="value"
+            data={distributions.workersDistribution}
+            outerRadius={160}
+            fill="#8884d8"
+          />
+          <Tooltip />
+        </PieChart>
       </div>
     </div>
   );
