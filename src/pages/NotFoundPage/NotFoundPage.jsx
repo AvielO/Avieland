3295fc8ff7a0bc63492ec36a197c4213d51df959
Aviel <aviell1100611@gmail.com>
2024-07-19 20:21:49 +0300

    Not found component content added

--- src/pages/NotFoundPage/NotFoundPage.jsx
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -1,5 +1,10 @@
 const NotFoundPage = () => {
-  return <div>Not Found</div>;
+  return (
+    <div>
+      <h1>404 - Page Not Found</h1>
+      <p>The page you are looking for does not exist.</p>
+    </div>
+  );
 };
 
 export default NotFoundPage;
