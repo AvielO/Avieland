37e89a065441719ff55845c5f9521342279ec5b4
Aviel <aviell1100611@gmail.com>
2024-07-20 01:55:41 +0300

    private route logic added to the component

--- src/components/PrivateRoute/PrivateRoute.jsx
+++ src/components/PrivateRoute/PrivateRoute.jsx
@@ -1,6 +1,10 @@
 import { Navigate } from "react-router-dom";
 
-const PrivateRoute = () => {
-    
+const isAuthenticated = () => {
+  return !!localStorage.getItem("username");
+};
+
+const PrivateRoute = ({ children }) => {
+  return isAuthenticated() ? children : <Navigate to="/signin" />;
 };
 export default PrivateRoute;
