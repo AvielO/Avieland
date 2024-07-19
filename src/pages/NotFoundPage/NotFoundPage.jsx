2e44dfddc0fd889b28e707d49cbda0d76d090154
Aviel <aviell1100611@gmail.com>
2024-07-19 20:30:22 +0300

    Not found component content edited

--- src/pages/NotFoundPage/NotFoundPage.jsx
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -1,8 +1,13 @@
+import { Link } from "react-router-dom";
+
 const NotFoundPage = () => {
   return (
-    <div>
-      <h1>404 - Page Not Found</h1>
-      <p>The page you are looking for does not exist.</p>
+    <div className="flex h-full flex-col items-center justify-center gap-2 text-5xl">
+      <h1 className="text-6xl">404</h1>
+      <h1>הדף שהגעת שניווטת אליו לא נמצא</h1>
+      <Link className="rounded-full bg-sky-200 p-4 text-2xl" to="/signin">
+        לחץ עליי כדי להתחבר
+      </Link>
     </div>
   );
 };
