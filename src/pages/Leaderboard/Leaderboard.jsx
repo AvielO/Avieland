6d7318824392f0ea9ae579dd4da1c3c1c7d564b3
Aviel <aviell1100611@gmail.com>
2024-07-21 02:10:30 +0300

    material ui added to project + circular progress to leaderboard page

--- src/pages/Leaderboard/Leaderboard.jsx
+++ src/pages/Leaderboard/Leaderboard.jsx
@@ -1,8 +1,10 @@
 import { useEffect, useState } from "react";
 import LeaderboardItem from "../../components/LeaderboardItem/LeaderboardItem";
 import { sortByAmount, sortByName } from "../../utils/columnSorts";
+import CircularProgress from "@mui/joy/CircularProgress";
 
 const Leaderboard = () => {
+  const [isLoading, setIsLoading] = useState(true);
   const [users, setUsers] = useState([]);
   const [activeSort, setActiveSort] = useState({
     columnName: "null",
@@ -36,12 +38,14 @@ const Leaderboard = () => {
 
   useEffect(() => {
     const fetchUsers = async () => {
+      setIsLoading(true);
       const res = await fetch(`${process.env.SERVER_URL}/users/leaderboard`);
       if (!res.ok) {
         throw new Error("Could not fetch the users");
       }
       const leaderboardUsers = await res.json();
       setUsers(leaderboardUsers);
+      setIsLoading(false);
     };
 
     fetchUsers();
@@ -51,70 +55,77 @@ const Leaderboard = () => {
   //Optional - sort of the list.DF
   return (
     <>
-      <table className="w-full">
-        <thead className="h-12 bg-sky-300 text-xl">
-          <tr className="cursor-pointer select-none text-sky-900">
-            <th
-              id="index"
-              className={`transition-all ${activeSort.columnName === "index" ? "bg-sky-500 text-sky-50" : ""}`}
-              onClick={(e) => handleSort(e.target.id)}
-            >
-              #
-            </th>
-            <th
-              id="username"
-              className={`transition-all ${activeSort.columnName === "username" ? "bg-sky-500 text-sky-50" : ""}`}
-              onClick={(e) => handleSort(e.target.id)}
-            >
-              שם משתמש
-            </th>
-            <th
-              id="type"
-              className={`transition-all ${activeSort.columnName === "type" ? "bg-sky-500 text-sky-50" : ""}`}
-              onClick={(e) => handleSort(e.target.id)}
-            >
-              סוג
-            </th>
-            <th
-              id="gold"
-              className={`transition-all ${activeSort.columnName === "gold" ? "bg-sky-500 text-sky-50" : ""}`}
-              onClick={(e) => handleSort(e.target.id)}
-            >
-              זהב
-            </th>
-            <th
-              id="soliders"
-              className={`transition-all ${activeSort.columnName === "soliders" ? "bg-sky-500 text-sky-50" : ""}`}
-              onClick={(e) => handleSort(e.target.id)}
-            >
-              לוחמים
-            </th>
-            <th
-              id="workers"
-              className={`transition-all ${activeSort.columnName === "workers" ? "bg-sky-500 text-sky-50" : ""}`}
-              onClick={(e) => handleSort(e.target.id)}
-            >
-              עובדים
-            </th>
-            <th className="cursor-default">אירגון</th>
-          </tr>
-        </thead>
-        <tbody className="bg-sky-100 text-2xl text-sky-700">
-          {users.map((user, index) => (
-            <LeaderboardItem
-              id={user.id}
-              index={user.index}
-              username={user.username}
-              type={user.type}
-              gold={user.gold}
-              soliders={user.soliders}
-              workers={user.workers}
-              key={user.id}
-              columnIndex={index}
-            />
-          ))}
-        </tbody>
-      </table>
+      {isLoading && (
+        <div className="flex items-center justify-center">
+          <CircularProgress size="lg" />
+        </div>
+      )}
+      {!isLoading && (
+        <table className="w-full">
+          <thead className="h-12 bg-sky-300 text-xl">
+            <tr className="cursor-pointer select-none text-sky-900">
+              <th
+                id="index"
+                className={`transition-all ${activeSort.columnName === "index" ? "bg-sky-500 text-sky-50" : ""}`}
+                onClick={(e) => handleSort(e.target.id)}
+              >
+                #
+              </th>
+              <th
+                id="username"
+                className={`transition-all ${activeSort.columnName === "username" ? "bg-sky-500 text-sky-50" : ""}`}
+                onClick={(e) => handleSort(e.target.id)}
+              >
+                שם משתמש
+              </th>
+              <th
+                id="type"
+                className={`transition-all ${activeSort.columnName === "type" ? "bg-sky-500 text-sky-50" : ""}`}
+                onClick={(e) => handleSort(e.target.id)}
+              >
+                סוג
+              </th>
+              <th
+                id="gold"
+                className={`transition-all ${activeSort.columnName === "gold" ? "bg-sky-500 text-sky-50" : ""}`}
+                onClick={(e) => handleSort(e.target.id)}
+              >
+                זהב
+              </th>
+              <th
+                id="soliders"
+                className={`transition-all ${activeSort.columnName === "soliders" ? "bg-sky-500 text-sky-50" : ""}`}
+                onClick={(e) => handleSort(e.target.id)}
+              >
+                לוחמים
+              </th>
+              <th
+                id="workers"
+                className={`transition-all ${activeSort.columnName === "workers" ? "bg-sky-500 text-sky-50" : ""}`}
+                onClick={(e) => handleSort(e.target.id)}
+              >
+                עובדים
+              </th>
+              <th className="cursor-default">אירגון</th>
+            </tr>
+          </thead>
+          <tbody className="bg-sky-100 text-2xl text-sky-700">
+            {users.map((user, index) => (
+              <LeaderboardItem
+                id={user.id}
+                index={user.index}
+                username={user.username}
+                type={user.type}
+                gold={user.gold}
+                soliders={user.soliders}
+                workers={user.workers}
+                key={user.id}
+                columnIndex={index}
+              />
+            ))}
+          </tbody>
+        </table>
+      )}
     </>
   );
 };
