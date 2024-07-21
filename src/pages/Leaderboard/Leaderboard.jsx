941a0041f79cff96be710abef9a67d6da9d6e75f
Aviel <aviell1100611@gmail.com>
2024-07-21 18:18:34 +0300

    page selection added + handle pagination

--- src/pages/Leaderboard/Leaderboard.jsx
+++ src/pages/Leaderboard/Leaderboard.jsx
@@ -4,7 +4,11 @@ import { sortByAmount, sortByName } from "../../utils/columnSorts";
 import CircularProgress from "@mui/joy/CircularProgress";
 import { fetchWrapper } from "../../utils/fetchWarpper";
 
+import { IoIosArrowBack } from "react-icons/io";
+import { IoIosArrowForward } from "react-icons/io";
+
 const Leaderboard = () => {
+  const [page, setPage] = useState(1);
   const [isLoading, setIsLoading] = useState(true);
   const [users, setUsers] = useState([]);
   const [activeSort, setActiveSort] = useState({
@@ -50,6 +54,10 @@ const Leaderboard = () => {
     fetchUsers();
   }, []);
 
+  const handlePagination = (direction) => {
+    console.log(direction);
+  };
+
   //UseEffect for fetch data of all players
   //Optional - sort of the list.DF
   return (
@@ -60,70 +68,87 @@ const Leaderboard = () => {
         </div>
       )}
       {!isLoading && (
-        <table className="w-full">
-          <thead className="h-12 bg-sky-300 text-xl">
-            <tr className="cursor-pointer select-none text-sky-900">
-              <th
-                id="index"
-                className={`transition-all ${activeSort.columnName === "index" ? "bg-sky-500 text-sky-50" : ""}`}
-                onClick={(e) => handleSort(e.target.id)}
-              >
-                #
-              </th>
-              <th
-                id="username"
-                className={`transition-all ${activeSort.columnName === "username" ? "bg-sky-500 text-sky-50" : ""}`}
-                onClick={(e) => handleSort(e.target.id)}
-              >
-                שם משתמש
-              </th>
-              <th
-                id="type"
-                className={`transition-all ${activeSort.columnName === "type" ? "bg-sky-500 text-sky-50" : ""}`}
-                onClick={(e) => handleSort(e.target.id)}
-              >
-                סוג
-              </th>
-              <th
-                id="gold"
-                className={`transition-all ${activeSort.columnName === "gold" ? "bg-sky-500 text-sky-50" : ""}`}
-                onClick={(e) => handleSort(e.target.id)}
-              >
-                זהב
-              </th>
-              <th
-                id="soliders"
-                className={`transition-all ${activeSort.columnName === "soliders" ? "bg-sky-500 text-sky-50" : ""}`}
-                onClick={(e) => handleSort(e.target.id)}
-              >
-                לוחמים
-              </th>
-              <th
-                id="workers"
-                className={`transition-all ${activeSort.columnName === "workers" ? "bg-sky-500 text-sky-50" : ""}`}
-                onClick={(e) => handleSort(e.target.id)}
-              >
-                עובדים
-              </th>
-              <th className="cursor-default">אירגון</th>
-            </tr>
-          </thead>
-          <tbody className="bg-sky-100 text-2xl text-sky-700">
-            {users.map((user, index) => (
-              <LeaderboardItem
-                id={user.id}
-                index={user.index}
-                username={user.username}
-                type={user.type}
-                gold={user.gold}
-                soliders={user.soliders}
-                workers={user.workers}
-                key={user.id}
-                columnIndex={index}
+        <div className="flex flex-col gap-6">
+          <table className="w-full">
+            <thead className="h-12 bg-sky-300 text-xl">
+              <tr className="cursor-pointer select-none text-sky-900">
+                <th
+                  id="index"
+                  className={`transition-all ${activeSort.columnName === "index" ? "bg-sky-500 text-sky-50" : ""}`}
+                  onClick={(e) => handleSort(e.target.id)}
+                >
+                  #
+                </th>
+                <th
+                  id="username"
+                  className={`transition-all ${activeSort.columnName === "username" ? "bg-sky-500 text-sky-50" : ""}`}
+                  onClick={(e) => handleSort(e.target.id)}
+                >
+                  שם משתמש
+                </th>
+                <th
+                  id="type"
+                  className={`transition-all ${activeSort.columnName === "type" ? "bg-sky-500 text-sky-50" : ""}`}
+                  onClick={(e) => handleSort(e.target.id)}
+                >
+                  סוג
+                </th>
+                <th
+                  id="gold"
+                  className={`transition-all ${activeSort.columnName === "gold" ? "bg-sky-500 text-sky-50" : ""}`}
+                  onClick={(e) => handleSort(e.target.id)}
+                >
+                  זהב
+                </th>
+                <th
+                  id="soliders"
+                  className={`transition-all ${activeSort.columnName === "soliders" ? "bg-sky-500 text-sky-50" : ""}`}
+                  onClick={(e) => handleSort(e.target.id)}
+                >
+                  לוחמים
+                </th>
+                <th
+                  id="workers"
+                  className={`transition-all ${activeSort.columnName === "workers" ? "bg-sky-500 text-sky-50" : ""}`}
+                  onClick={(e) => handleSort(e.target.id)}
+                >
+                  עובדים
+                </th>
+                <th className="cursor-default">אירגון</th>
+              </tr>
+            </thead>
+            <tbody className="bg-sky-100 text-2xl text-sky-700">
+              {users.map((user, index) => (
+                <LeaderboardItem
+                  id={user.id}
+                  index={user.index}
+                  username={user.username}
+                  type={user.type}
+                  gold={user.gold}
+                  soliders={user.soliders}
+                  workers={user.workers}
+                  key={user.id}
+                  columnIndex={index}
+                />
+              ))}
+            </tbody>
+          </table>
+          <div className="flex w-full items-center justify-center text-3xl">
+            <div className="flex w-fit items-center gap-4 rounded-full bg-sky-50">
+              <IoIosArrowForward
+                value="forward"
+                className="cursor-pointer"
+                onClick={() => handlePagination("forward")}
               />
-            ))}
-          </tbody>
-        </table>
+              <span className="text-3xl">{page}</span>
+              <IoIosArrowBack
+                value="back"
+                className="cursor-pointer"
+                onClick={() => handlePagination("back")}
+              />
+            </div>
+          </div>
+        </div>
       )}
     </>
   );
